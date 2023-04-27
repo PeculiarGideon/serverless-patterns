d034602233d7500fc1b7eaf8acb4773ef0a417c3
Vikrant,Dhir <vikdhir@amazon.com>
2023-04-27 16:27:03 -0400

    AWS service template using Terraform open source requires the files to be tarballed ( in .tar.gz format).Added tarball versions of terraform folders so that they can be imported into the service-template as individual prodcts"

---
+++ apigw-custom-domain-edge/tarball_terraform_patterns.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+: <<'COMMENT'
+AWS service template using Terraform open source requires the files to be tarballed ( in .tar.gz format). 
+This bash script identifies all the folders with terraform patterns and then tarballs these folders so that they can be imported into the service-template as individual prodcts"
+
+note tarball should be created by cd ing into the pattern folder
+
+COMMENT
+
+
+
+for dir in *terraform*
+do
+ cd $dir 
+ echo $dir
+ tar -czvf $dir.tar.gz .
+ cd ..
+done
